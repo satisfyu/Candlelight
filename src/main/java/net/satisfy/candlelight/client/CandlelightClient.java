14c8ad8062d85bd1b751ca49a5a28592066aa3a3
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-01-21 19:06:51 +0100

    cooking hat

--- src/main/java/net/satisfy/candlelight/client/CandlelightClient.java
+++ src/main/java/net/satisfy/candlelight/client/CandlelightClient.java
@@ -2,7 +2,9 @@ package net.satisfy.candlelight.client;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.minecraft.client.render.RenderLayer;
+import net.satisfy.candlelight.client.model.CookingHatModel;
 import net.satisfy.candlelight.registry.ObjectRegistry;
 
 public class CandlelightClient implements ClientModInitializer {
@@ -10,5 +12,10 @@ public class CandlelightClient implements ClientModInitializer {
     public void onInitializeClient() {
         BlockRenderLayerMap.INSTANCE.putBlocks(RenderLayer.getCutout(), ObjectRegistry.CAKE_STAND
         );
+        registerModels();
+    }
+
+    private static void registerModels(){
+        EntityModelLayerRegistry.registerModelLayer(CookingHatModel.LAYER_LOCATION, CookingHatModel::getTexturedModelData);
     }
 }
