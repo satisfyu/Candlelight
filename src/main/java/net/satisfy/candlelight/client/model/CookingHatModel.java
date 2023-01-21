8dc70a0506ccd2817e5e19221a0a216e87ede9a5
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-01-21 20:12:28 +0100

    cooking hat

--- src/main/java/net/satisfy/candlelight/client/model/CookingHatModel.java
+++ src/main/java/net/satisfy/candlelight/client/model/CookingHatModel.java
@@ -18,11 +18,12 @@ public class CookingHatModel<T extends Entity> extends EntityModel<T> {
 	public static TexturedModelData getTexturedModelData() {
 		ModelData modelData = new ModelData();
 		ModelPartData modelPartData = modelData.getRoot();
-		ModelPartData cookhat = modelPartData.addChild("cookhat", ModelPartBuilder.create(), ModelTransform.pivot(7.875F, 24.0F, -7.875F));
+		ModelPartData cookhat = modelPartData.addChild("cookhat", ModelPartBuilder.create(), ModelTransform.pivot(7.875F, 23.0F, -7.875F));
 
-		ModelPartData top_part = cookhat.addChild("top_part", ModelPartBuilder.create().uv(1, 1).cuboid(-5.0F, -11.0F, -5.0F, 10.0F, 6.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(-7.875F, 0.0F, 7.875F));
+		ModelPartData top_part = cookhat.addChild("top_part", ModelPartBuilder.create().uv(1, 1).cuboid(-5.0F, -10.0F, -5.0F, 10.0F, 6.0F, 10.0F, new Dilation(0.0F)), ModelTransform.pivot(-7.875F, 0.0F, 7.875F));
 
-		ModelPartData bottom_part = cookhat.addChild("bottom_part", ModelPartBuilder.create().uv(1, 21).cuboid(-4.0F, -5.0F, -4.0F, 8.0F, 5.0F, 8.0F, new Dilation(0.0F)), ModelTransform.pivot(-7.875F, 0.0F, 7.875F));
+		ModelPartData bottom_part = cookhat.addChild("bottom_part", ModelPartBuilder.create().uv(1, 21).cuboid(-4.0F, -4.0F, -4.0F, 8.0F, 4.0F, 8.0F, new Dilation(0.0F))
+				.uv(4, 2).cuboid(-4.5F, -0.9F, -4.5F, 9.0F, 1.0F, 9.0F, new Dilation(0.0F)), ModelTransform.pivot(-7.875F, 0.0F, 7.875F));
 		return TexturedModelData.of(modelData, 64, 64);
 	}
 	@Override
