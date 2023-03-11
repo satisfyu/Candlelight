4599f42ef95f95d808815e0f2770090007fd5a99
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-03-11 14:49:28 +0100

    Update Candlelight.java

--- src/main/java/net/satisfy/candlelight/Candlelight.java
+++ src/main/java/net/satisfy/candlelight/Candlelight.java
@@ -24,111 +24,7 @@ import static net.moddingplayground.frame.api.tabbeditemgroups.v0.Tab.Predicate.
 public class Candlelight implements ModInitializer {
 	public static final String MOD_ID = "candlelight";
 	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
-/*
-	public static final ItemGroup ITEM_GROUP_TABBED_ICON_TEXTURES =
-			TabbedItemGroup.builder()
-					.defaultPredicate(Tab.Predicate.CONTAINS)
-					.tab(Tab.builder().predicate(Tab.Predicate.items(
-							ObjectRegistry.TOMATO_SEEDS,
-							ObjectRegistry.TOMATO,
-							ObjectRegistry.TOMATO_CRATE,
-							ObjectRegistry.BROCCOLI_SEEDS,
-							ObjectRegistry.BROCCOLI,
-							ObjectRegistry.BROCCOLI_CRATE,
-							ObjectRegistry.STRAWBERRY_SEEDS,
-							ObjectRegistry.STRAWBERRY,
-							ObjectRegistry.STRAWBERRY_CRATE,
-							ObjectRegistry.SOFA,
-							ObjectRegistry.CHAIR,
-							ObjectRegistry.TABLE,
-							ObjectRegistry.LAMP,
-							ObjectRegistry.WOOD_FIRED_OVEN,
-							ObjectRegistry.STOVE,
-							ObjectRegistry.SIDEBOARD,
-							ObjectRegistry.DRAWER,
-							ObjectRegistry.CABINET,
-							ObjectRegistry.KITCHEN_SINK,
-							ObjectRegistry.FLOORBOARD,
-							ObjectRegistry.CAKE_STAND,
-							ObjectRegistry.COOKING_POT,
-							ObjectRegistry.COOKING_PAN,
-							ObjectRegistry.TRAY,
-							ObjectRegistry.TABLE_SET,
-							ObjectRegistry.CAKE_STAND,
-							ObjectRegistry.COOKING_POT,
-							ObjectRegistry.BUTTER,
-							ObjectRegistry.CHOCOLATE,
-							ObjectRegistry.MOZZARELLA,
-							ObjectRegistry.TOMATO_SOUP,
-							ObjectRegistry.BEETROOT_SALAD,
-							ObjectRegistry.COOKED_BEEF,
-							ObjectRegistry.PASTA,
-							ObjectRegistry.BEEF_TARTARE,
-							//ObjectRegistry.TOMATO_MOZZARELLA_SALAT,
-							ObjectRegistry.BROCCOLI_BEEF,
-							ObjectRegistry.BROCCOLI_TOMATO,
-							//ObjectRegistry.ROASTBEEF_CARROTS,
-							ObjectRegistry.SALMON,
-							ObjectRegistry.VEGGIE_PLATE,
-							ObjectRegistry.PORK_RIB,
-							//ObjectRegistry.LASAGNA,
-							//ObjectRegistry.BEEF_WELLINGTON,
-							ObjectRegistry.MASHED_POTATOES,
-							ObjectRegistry.FRICASSE,
-							ObjectRegistry.FRIED_EGG,
-							ObjectRegistry.STRAWBERRY_JAM,
-							ObjectRegistry.VINEGAR,
-							ObjectRegistry.RED_WINE,
-							ObjectRegistry.PRAETORIAN_WINE,
-							ObjectRegistry.PANCAKE,
-							ObjectRegistry.WAFFLE,
-							ObjectRegistry.STRAWBERRY_GLAZED_COOKIE,
-							ObjectRegistry.SWEETBERRY_GLAZED_COOKIE,
-							ObjectRegistry.CHOCOLATE_GLAZED_COOKIE,
-							ObjectRegistry.CROISSANT,
-							ObjectRegistry.BUNDT_CAKE,
-							ObjectRegistry.SESAM_BREAD,
-							ObjectRegistry.STRAWBERRY_CHOCOLATE,
-							ObjectRegistry.STRAWBERRY_CAKE_SLICE,
-							ObjectRegistry.STRAWBERRY_CAKE,
-							ObjectRegistry.STRAWBERRY_MILKSHAKE,
-							ObjectRegistry.STRAWBERRY_ICECREAM,
-							ObjectRegistry.SWEETBERRY_CAKE_SLICE,
-							ObjectRegistry.SWEETBERRY_CAKE,
-							ObjectRegistry.SWEETBERRY_MILKSHAKE,
-							ObjectRegistry.SWEETBERRY_ICECREAM,
-							ObjectRegistry.CHOCOLATE_CAKE_SLICE,
-							ObjectRegistry.CHOCOLATE_CAKE,
-							ObjectRegistry.CHOCOLATE_MILKSHAKE,
-							ObjectRegistry.CHOCOLATE_ICECREAM,
-							ObjectRegistry.TABLE_SIGN,
-							ObjectRegistry.STREET_SIGN,
-							ObjectRegistry.PAINTING,
-							ObjectRegistry.HEARTH,
-							ObjectRegistry.ROSE,
-							ObjectRegistry.CANDLE,
-							ObjectRegistry.JEWELRY_BOX,
-							ObjectRegistry.CHOCOLATE_BOX,
-							ObjectRegistry.COOKING_HAT,
-							ObjectRegistry.CHEFS_JACKET,
-							ObjectRegistry.CHEFS_PANTS,
-							ObjectRegistry.CHEFS_BOOTS,
-							ObjectRegistry.GLASS,
-							ObjectRegistry.NAPKIN,
-							ObjectRegistry.BOOK,
-							ObjectRegistry.NOTE_PAPER,
-							ObjectRegistry.NOTE_PAPER_WRITEABLE,
-							ObjectRegistry.NOTE_PAPER_WRITTEN,
-							ObjectRegistry.TYPEWRITER_COPPER,
-							ObjectRegistry.TYPEWRITER_IRON,
-							ObjectRegistry.LETTER_OPEN,
-							ObjectRegistry.LETTER_CLOSED,
-							ObjectRegistry.LOVE_LETTER
-					)).build("candlelight", GUIIcon.of(() -> new ItemStack(ObjectRegistry.HEARTH))))
 
-					.build(new VineryIdentifier("vinery_tab"), g -> GUIIcon.of(() -> new ItemStack(satisfyu.vinery.registry.ObjectRegistry.RED_GRAPE)));
-
- */
 	@Override
 	public void onInitialize() {
 		RecipeTypes.init();
@@ -140,14 +36,13 @@ public class Candlelight implements ModInitializer {
 				Tab.Predicate.items(
 				ObjectRegistry.TOMATO_SEEDS,
 				ObjectRegistry.TOMATO,
-				/*ObjectRegistry.TOMATO_CRATE,
+				ObjectRegistry.TOMATO_CRATE,
 				ObjectRegistry.BROCCOLI_SEEDS,
 				ObjectRegistry.BROCCOLI,
 				ObjectRegistry.BROCCOLI_CRATE,
 				ObjectRegistry.STRAWBERRY_SEEDS,
 				ObjectRegistry.STRAWBERRY,
 				ObjectRegistry.STRAWBERRY_CRATE,
-
 				ObjectRegistry.SOFA,
 				ObjectRegistry.CHAIR,
 				ObjectRegistry.TABLE,
@@ -224,14 +119,14 @@ public class Candlelight implements ModInitializer {
 				ObjectRegistry.BOOK,
 				ObjectRegistry.NOTE_PAPER,
 				ObjectRegistry.NOTE_PAPER_WRITEABLE,
-				ObjectRegistry.NOTE_PAPER_WRITTEN,*/
-				//ObjectRegistry.TYPEWRITTER_IRON,
-				//ObjectRegistry.TYPEWRITTER_COPPER,
+				ObjectRegistry.NOTE_PAPER_WRITTEN,
+				ObjectRegistry.TYPEWRITER_IRON,
+				ObjectRegistry.TYPEWRITER_COPPER,
 				ObjectRegistry.LETTER_OPEN,
 				ObjectRegistry.LETTER_CLOSED,
 				ObjectRegistry.LOVE_LETTER
 
-		)).build("candlelight", GUIIcon.of(() -> new ItemStack(ObjectRegistry.TOMATO)));
+		)).build("candlelight", GUIIcon.of(() -> new ItemStack(ObjectRegistry.HEARTH)));
 		((TabbedItemGroup) Vinery.CREATIVE_TAB).getTabs().add(tab);
 		tab.addToGroup(((TabbedItemGroup) Vinery.CREATIVE_TAB));
 
