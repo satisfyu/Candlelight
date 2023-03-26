644b867fff02f7b4db233666f85660bb270461a4
satisfyu <104036234+satisfyu@users.noreply.github.com>
2023-03-26 23:23:25 +0200

    small fixed

--- src/main/java/net/satisfy/candlelight/food/Feast.java
+++ src/main/java/net/satisfy/candlelight/food/Feast.java
@@ -1,7 +1,19 @@
 package net.satisfy.candlelight.food;
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.satisfy.candlelight.registry.ObjectRegistry;
+
 public class Feast extends CandlelightFood {
     public Feast(Settings settings) {
         super(settings);
     }
+
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack itemStack = super.finishUsing(stack, world, user);
+        return user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode ? itemStack : new ItemStack(ObjectRegistry.TRAY);
+    }
 }
+
--- src/main/java/net/satisfy/candlelight/food/Meal.java
+++ src/main/java/net/satisfy/candlelight/food/Meal.java
@@ -1,11 +1,21 @@
 package net.satisfy.candlelight.food;
 
 
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+import net.satisfy.candlelight.registry.ObjectRegistry;
+
 public class Meal extends CandlelightFood {
 
     public Meal(Settings settings) {
         super(settings);
     }
 
-
+    public ItemStack finishUsing(ItemStack stack, World world, LivingEntity user) {
+        ItemStack itemStack = super.finishUsing(stack, world, user);
+        return user instanceof PlayerEntity && ((PlayerEntity)user).getAbilities().creativeMode ? itemStack : new ItemStack(ObjectRegistry.TRAY);
+    }
 }
+
