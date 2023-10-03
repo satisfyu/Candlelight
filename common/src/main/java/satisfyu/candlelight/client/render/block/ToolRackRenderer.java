fde4286a895e251d0d31fd36a77f2d53971fa420
Cristelknight999 <54945257+Cristelknight999@users.noreply.github.com>
2023-10-03 11:25:32 +0200

    Update ToolRackRenderer.java

--- common/src/main/java/satisfyu/candlelight/client/render/block/ToolRackRenderer.java
+++ common/src/main/java/satisfyu/candlelight/client/render/block/ToolRackRenderer.java
@@ -18,22 +18,23 @@ import satisfyu.candlelight.registry.ObjectRegistry;
 public class ToolRackRenderer implements StorageTypeRenderer {
     @Override
     public void render(StorageBlockEntity entity, PoseStack matrices, MultiBufferSource vertexConsumers, NonNullList<ItemStack> itemStacks) {
-        for (int i = 0; i < Math.min(itemStacks.size(), 4); i++) {
+        for(int i = 0; i < itemStacks.size(); i++) {
             ItemStack stack = itemStacks.get(i);
 
             if (!stack.isEmpty()) {
-                Item item = stack.getItem();
-
                 matrices.pushPose();
+                Item item = stack.getItem();
+                double translate = (i + 1) * (1D / 3);
+
                 if (item instanceof BlockItem blockItem && blockItem.getBlock() == ObjectRegistry.COOKING_PAN.get()) {
-                    matrices.translate(0f, 0.8f, 0.45f);
+                    matrices.translate(translate - 1, 0.8f, 0.45f);
                     matrices.scale(0.6f, 0.6f, 0.6f);
                     matrices.mulPose(Vector3f.ZN.rotationDegrees(-85f));
                     matrices.mulPose(Vector3f.XN.rotationDegrees(90f));
                     matrices.mulPose(Vector3f.YN.rotationDegrees(180f));
                     ClientUtil.renderBlockFromItem(blockItem, matrices, vertexConsumers, entity);
                 } else {
-                    matrices.translate(0f, 0.6f, 0.38f);
+                    matrices.translate(translate - (2D / 3), 0.6f, 0.38f);
                     matrices.scale(0.6f, 0.6f, 0.6f);
                     matrices.mulPose(Vector3f.ZN.rotationDegrees(135f));
                     matrices.mulPose(Vector3f.YN.rotationDegrees(0f));
